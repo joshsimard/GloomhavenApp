c9cc1443a83c2c5f397b2ed743852cf26c27e6b3
joshsimard <joshuasimard@gmail.com>
2018-01-09 00:34:18 -0600

    renaming things to be more appropriate to app; added pictures; changed how the home page works a bit;

--- src/providers/providers.ts
+++ src/providers/providers.ts
@@ -1,11 +1,13 @@
 import { Api } from './api/api';
 import { Items } from '../mocks/providers/items';
+import { Characters } from '../mocks/providers/characters';
 import { Settings } from './settings/settings';
 import { User } from './user/user';
 
 export {
     Api,
     Items,
+    Characters,
     Settings,
     User
 };
